8371fa636a182227f2e6620f001ab368695fa8aa
risaleo <iloveleo7@gmail.com>
2026-08-10 22:41:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const C='reizo-v22-cache';const A=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./assets/home-guide.png", "./assets/compare-guide.png", "./assets/basic-guide.png", "./assets/capacity-guide.png", "./assets/install-guide.png", "./assets/sales-guide.png", "./assets/quiz-guide.png"];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='reizo-v23-cache';
+const A=['./manifest.webmanifest','./icon-192.png','./icon-512.png','./assets/home-guide.png','./assets/compare-guide.png','./assets/basic-guide.png','./assets/capacity-guide.png','./assets/install-guide.png','./assets/sales-guide.png','./assets/quiz-guide.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate' || new URL(e.request.url).pathname.endsWith('/index.html')){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
