ca8b58ab68bdd5ff47c7f7733261c01ff934db86
risaleo <iloveleo7@gmail.com>
2026-08-11 20:26:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-const C='reizo-v24-assets';
-const ASSETS=['./home-guide.png','./compare-guide.png','./basic-guide.png','./capacity-guide.png','./install-guide.png','./sales-guide.png','./quiz-guide.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));return;}
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();if(e.request.url.includes('/assets/')||e.request.url.includes('icon-'))caches.open(C).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
-});
+const C='reizo-v25-flat-cache';const A=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./compare-guide.png", "./basic-guide.png", "./capacity-guide.png", "./install-guide.png", "./sales-guide.png", "./quiz-guide.png"];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
